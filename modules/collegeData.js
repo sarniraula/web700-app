c7fa7d44664e5c558c72554430b366af0f5bed77
sarniraula <sar.niraula162@gmail.com>
2024-06-14 12:47:22 -0400

    first commit

---
+++ modules/collegeData.js
@@ -0,0 +1,96 @@
+const fs = require('fs');      //to import the fs module
+
+class Data {                    
+    constructor(students, courses) {
+        this.students = students;
+        this.courses = courses;
+    }
+}
+
+let dataCollection = null;
+
+const initialize = () => {
+    return new Promise((resolve, reject) => { 
+        fs.readFile('./data/students.json', 'utf8', (err, studentData) => {        // When we read files asynchronously (which is what fs.readFile does),  
+            if (err) {                                                             //it means that it starts reading the file, 
+                reject("Unable to read students.json");                            //but we don't wait around for it to finish. Instead, 
+                return;                                                            //we give it a function to call when it's done (the callback function).
+            }
+            let students = JSON.parse(studentData);
+
+            fs.readFile('./data/courses.json', 'utf8', (err, courseData) => {       //nesting fs.readfile so that the second file read only starts after the first one has completed.
+                if (err) {
+                    reject("Unable to read courses.json");
+                    return;
+                }
+                let courses = JSON.parse(courseData);
+                dataCollection = new Data(students, courses);       //This only runs after fs.readfile job is completed.
+                resolve();
+            });
+        });
+    });
+};
+
+const getAllStudents = () => {
+    return new Promise((resolve, reject) => {
+        if (dataCollection.students.length === 0) {
+            reject("No results.");
+        } else {
+            resolve(dataCollection.students);
+        }
+    });
+};
+
+const getTAs = () => {
+    return new Promise((resolve, reject) => {
+        const TAs = dataCollection.students.filter(student => student.TA === true);
+        if (TAs.length === 0) {
+            reject("No results.");
+        } else {
+            resolve(TAs);
+        }
+    });
+};
+
+const getCourses = () => {
+    return new Promise((resolve, reject) => {
+        if (dataCollection.courses.length === 0) {
+            reject("no results returned");
+        } else {
+            resolve(dataCollection.courses);
+        }
+    });
+};
+
+const getStudentsByCourse = (course) => {
+
+    const selectedStudents = dataCollection.students.filter( student => student.course === parseInt(course));
+
+    return new Promise((resolve, reject) => {
+        if (selectedStudents.length > 0) {
+            resolve(selectedStudents);
+        } else {
+            reject('No results returned');
+        }
+    })
+}
+
+const getStudentsByNum = (num) => {
+    const selectedStudents = dataCollection.students.filter( student => student.studentNum === num);
+    return new Promise((resolve, reject) => {
+        if (selectedStudents.length > 0) {
+            resolve(selectedStudents);
+        } else {
+            reject('No results returned');
+        }
+    })
+}
+
+module.exports = {
+    initialize,
+    getAllStudents,
+    getTAs,
+    getCourses,
+    getStudentsByCourse,
+    getStudentsByNum
+};
---
+++ server.js
@@ -0,0 +1,76 @@
+/*********************************************************************************
+* WEB700 – Assignment 03
+* I declare that this assignment is my own work in accordance with Seneca Academic Policy. No part
+* of this assignment has been copied manually or electronically from any other source
+* (including 3rd party web sites) or distributed to other students.
+*
+* Name: Saroj Niraula Student ID: 154184238 Date: 6/14/2024
+*
+********************************************************************************/
+
+var HTTP_PORT = process.env.PORT || 8080;
+var express = require("express");
+const path = require('path');
+var app = express();
+
+const collegeData = require('./modules/collegeData');
+
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'home.html'));
+});
+
+app.get("/about", (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'about.html'));
+});
+
+app.get("/htmlDemo", (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'htmlDemo.html'));
+});
+
+
+app.get("/students", (req, res) => {
+    if (req.query.course) {
+        collegeData.getStudentsByCourse(req.query.course)
+            .then((students) => {
+                res.json(students);
+            })
+            .catch((err) => {
+                res.json({ message: err });
+            });
+    } else {
+        collegeData.getAllStudents()
+            .then((students) => {   
+                res.json(students);
+            })
+            .catch((err) => {
+                res.json({ message: "No results" });
+            });
+    }
+});
+
+app.get("/tas", (req, res) => {
+    collegeData.getTAs()
+        .then((tas) => {
+            res.json(tas);
+        })
+        .catch((err) => {
+            res.json({ message: "No results" });
+        });
+});
+
+app.get('/courses', (req, res) => {
+    collegeData.getCourses().then(data => {
+        res.json(data);
+    }).catch(err => {
+        res.json({ message: err });
+    });
+});
+
+collegeData.initialize()
+    .then(() => {
+        app.listen(HTTP_PORT, ()=> {console.log("Server listening on port "+ HTTP_PORT)});
+    })
+    .catch((err) => {
+        console.error(err);
+    });
